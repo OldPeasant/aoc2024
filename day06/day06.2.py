99b0007851ac5d76d01cf69e4213760cea900fd3
Peter Gürtler <peter.guertler@ausy.ch>
2024-12-09 12:52:44 +0100

    Day 6

--- day06/day06.2.py
+++ day06/day06.2.py
@@ -48,8 +48,9 @@ def has_loop(safe_lines, start_row, start_col):
         next_row = row + direction[0]
         next_col = col + direction[1]
         if next_row == 0 or next_col == 0 or next_row == height -1 or next_col == width - 1:
+            #print("This went outside")
             break
-        if safe_lines[next_row][next_col] == '#':
+        if safe_lines[next_row][next_col] == '#' or safe_lines[next_row][next_col] == 'O':
             direction = TURN[direction]
         else:
             row = next_row
@@ -57,21 +58,23 @@ def has_loop(safe_lines, start_row, start_col):
             #print(direction, row, col)
         st = (row, col, direction[0], direction[1])
         if st in past:
-            print("Loop:")
+            #print("Loop:", st)
             for p in past:
                 if p[0] != start_row or p[1] != start_col:
+                    if safe_lines[p[0]][p[1]] == 'O':
+                        raise Exception()
                     safe_lines[p[0]][p[1]] = "+"
             
-            for l in safe_lines:
-                print(l)
+            #for l in safe_lines:
+            #    print("".join(l))
             return True
         past.add(st)
-    print("no loop")
+    #print("no loop")
     for p in past:
         if p[0] != start_row or p[1] != start_col:
             safe_lines[p[0]][p[1]] = "+"
-    for l in safe_lines:
-        print(l)
+    #for l in safe_lines:
+    #    print("".join(l))
     return False
 
 with open(sys.argv[1]) as f:
@@ -82,13 +85,13 @@ with open(sys.argv[1]) as f:
 #    safe_lines[row][col] = '.'
 
     count_loops = 0
-    for obst_row in range(1, len(safe_lines) - 2):
-        for obst_col in range(1, len(safe_lines[0]) - 2):
+    for obst_row in range(1, len(safe_lines) - 1):
+        for obst_col in range(1, len(safe_lines[0]) - 1):
             safe_lines = border(lines)
             if obst_row != row or obst_col != col:
                 safe_lines[obst_row][obst_col] = "O"
                 if has_loop(safe_lines, row, col):
                     count_loops += 1
-            print(obst_row, obst_col, count_loops)
+            #print(obst_row, obst_col, count_loops)
     print(count_loops)
 
